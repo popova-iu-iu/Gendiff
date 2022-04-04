48ccf9c08e0eb6008a518d64d0dba4e3eee49102
iuliia <popova.iuliia.iu@gmail.com>
2022-04-04 18:07:35 +0300

    add option and argument to gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,8 @@ const program = new Command();
 program
 
   .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>, <filepath2>')
 
   .description('Compares two configuration files and shows a difference.');
 
