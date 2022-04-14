414c19470e064512d59f847fd1b7138aabdab54c
iuliia <93525073+popova-iu-iu@users.noreply.github.com>
2022-04-14 16:36:42 +0300

    update gendiff_test.js

--- __tests__/gendiff_test.js
+++ __tests__/gendiff_test.js
@@ -1,18 +1,1 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import genDiff from '../src/index.js';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-
-test('check diff', () => {
-  const file1Path = getFixturePath('file1.json');
-  const file2Path = getFixturePath('file2.json');
-
-  const actual = genDiff(file1Path, file2Path);
-  const plain = readFile('plain.txt');
-  expect(actual).toEqual(plain);
-});
