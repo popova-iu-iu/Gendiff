5152c2d15bb70b7ebe28e43c8caecacb3a1c3b7d
iuliia <popova.iuliia.iu@gmail.com>
2022-04-18 23:37:02 +0300

    fixed gendiff

--- src/index.js
+++ src/index.js
@@ -1,34 +1,35 @@
-import fs from 'fs';
 import _ from 'lodash';
-import { isAbsolute, resolve } from 'path';
+import path, { isAbsolute, resolve } from 'path';
+import fs, { readFileSync } from 'fs';
+import { cwd } from 'process';
 
 const parseFile = (filepath) => {
-  const path = isAbsolute(filepath) ? filepath : resolve(filepath);
-  const file = fs.readFileSync(path, 'utf-8');
-  return JSON.parse(file);
-};
+    const path = isAbsolute(filepath) ? filepath : resolve(filepath);
+    const file = fs.readFileSync(path, 'utf-8');
+    return JSON.parse(file);
+}
 
 const genDiff = (filepath1, filepath2) => {
-  const file1 = parseFile(filepath1);
-  const file2 = parseFile(filepath2);
-  const commomObject = { ...file2, ...file1 };
+    const file1 = parseFile(filepath1);
+    const file2 = parseFile(filepath2);
+    const keys = Object.keys({ ...file1, ...file2 });
 
-  const lines = _.sortBy(Object.entries(commomObject))
-    .reduce((acc, [key, value]) => {
-      if (file1[key] === file2[key]) {
-        acc[`  ${key}`] = value;
-      } else if (file1[key] !== file2[key]) {
-        acc[`- ${key}`] = file1[key];
-        acc[`+ ${key}`] = file2[key];
-      } else if (_.has(file1, key)) {
-        acc[`- ${key}`] = value;
-      } else if (_.has(file2, key)) {
-        acc[`+ ${key}`] = value;
-      }
-      return acc;
-    }, {});
-  const result = JSON.stringify(lines, '/n', 2);
-  return result.replace(/"/g, '').replace(/,/g, '');
-};
+    const result = _.sortBy(keys, (key) => key)
+    .map((key) => {        
+        if (!_.has(file2, key)) {
+            return `- ${key}: ${file1[key]}`;
+          }
+        if (!_.has(file1, key)) {
+            return `+ ${key}: ${file2[key]}`;
+        }
+        if (file1[key] === file2[key]) {            
+            return `  ${key}: ${file1[key]}`;
+        }
+        return `- ${key}: ${file1[key]}\n + ${key}: ${file2[key]}`;     
+       })
+    .join('\n ')
+    return `{\n ${result}\n}`;
+}
 
 export default genDiff;
+
