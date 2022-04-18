a0c530143ca092e0d343f2528889ec46386f3c17
iuliia <popova.iuliia.iu@gmail.com>
2022-04-18 23:37:31 +0300

    add tests

---
+++ __tests__/test.js
@@ -0,0 +1,15 @@
+
+import genDiff from '../src/index.js';
+
+const result = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+test('genDiff', () => {
+  expect(genDiff(filepath1, filepath2)).toBe(result);
+});
