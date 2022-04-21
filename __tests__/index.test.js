807d542cbfedbafd1fb45d7f8f25134b5a841a62
iuliia <popova.iuliia.iu@gmail.com>
2022-04-21 10:38:19 +0300

    fixed tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,12 +3,12 @@ import genDiff from '../src/index.js';
 const filepath1 = './__fixtures__/file1.json';
 const filepath2 = './__fixtures__/file2.json';
 const result = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+ - follow: false
+   host: hexlet.io
+ - proxy: 123.234.53.22
+ - timeout: 50
+ + timeout: 20
+ + verbose: true
 }`;
 
 test('genDiff', () => {
