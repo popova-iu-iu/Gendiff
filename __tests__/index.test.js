7b0ff2e685fbcb9dfb819cb19faecc19e0e01cf4
iuliia <popova.iuliia.iu@gmail.com>
2022-05-01 21:13:55 +0300

    add test yml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,17 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
 import genDiff from '../src/index.js';
 
-const filepath1 = './__fixtures__/file1.json';
-const filepath2 = './__fixtures__/file2.json';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const filepath1 = getFixturePath('file1.json');
+const filepath2 = getFixturePath('file2.json');
+const filepath3 = getFixturePath('file3.yml');
+const filepath4 = getFixturePath('file4.yml');
+
 const result = `{
  - follow: false
    host: hexlet.io
@@ -13,4 +23,5 @@ const result = `{
 
 test('genDiff', () => {
   expect(genDiff(filepath1, filepath2)).toBe(result);
-}); // test gendiff
+  expect(genDiff(filepath3, filepath4)).toBe(result);
+});
