e52a8d1a04e67e4f20849d8a1ed55faa3a50c7e2
iuliia <popova.iuliia.iu@gmail.com>
2022-05-01 21:15:25 +0300

    add yml parse

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import _ from 'lodash';
-import { isAbsolute, resolve } from 'path';
-import fs from 'fs';
+import * as path from 'path';
+import { readFileSync } from 'fs';
+import parse from './parsers.js';
 
-const parseFile = (filepath) => {
-  const path = isAbsolute(filepath) ? filepath : resolve(filepath);
-  const file = fs.readFileSync(path, 'utf-8');
-  return JSON.parse(file);
+// const parseFile = (filepath) => {
+//   const path = isAbsolute(filepath) ? filepath : resolve(filepath);
+//   const file = fs.readFileSync(path, 'utf-8');
+//   return parse(file);
+// };
+
+const prepareData = (file) => readFileSync(path.resolve(file));
+
+const getData = (file) => {
+  const readyData = prepareData(file);
+  const parseMethod = parse(file);
+
+  return parseMethod(readyData);
 };
 
 const genDiff = (filepath1, filepath2) => {
-  const file1 = parseFile(filepath1);
-  const file2 = parseFile(filepath2);
+  const file1 = getData(filepath1);
+  const file2 = getData(filepath2);
   const keys = Object.keys({ ...file1, ...file2 });
 
   const result = _.sortBy(keys, (key) => key)
