0ad2a8322f50de10d5a66403d7d3d7b57f05f17e
iuliia <popova.iuliia.iu@gmail.com>
2022-05-28 21:51:45 +0300

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { readFileSync } from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -7,21 +8,10 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const filepath1 = getFixturePath('file1.json');
-const filepath2 = getFixturePath('file2.json');
-const filepath3 = getFixturePath('file3.yml');
-const filepath4 = getFixturePath('file4.yml');
-
-const result = `{
- - follow: false
-   host: hexlet.io
- - proxy: 123.234.53.22
- - timeout: 50
- + timeout: 20
- + verbose: true
-}`;
-
 test('genDiff', () => {
-  expect(genDiff(filepath1, filepath2)).toBe(result);
-  expect(genDiff(filepath3, filepath4)).toBe(result);
+  const expected = readFileSync(getFixturePath('diffTest'), 'utf-8');
+  const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  console.log(expected);
+  console.log(result)
+  expect(result).toEqual(expected); 
 });
