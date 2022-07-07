5b725b61b121d2d08604d99f88381da971b0e391
iuliia <popova.iuliia.iu@gmail.com>
2022-07-07 18:36:24 +0300

    fix parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,16 @@
 import yaml from 'js-yaml';
 
-export default {
-  '.json': JSON.parse,
-  '.yaml': yaml.load,
-  '.yml': yaml.load,
+const parsers = (data, format) => {
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yml':
+      return yaml.load(data);
+    case 'yaml':
+      return yaml.load(data);
+    default:
+      throw new Error(`Unknown format: ${format}`);
+  }
 };
+
+export default parsers;
