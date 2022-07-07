c536aa85d566055f5f7ae8ad784a8d06dd660431
iuliia <popova.iuliia.iu@gmail.com>
2022-07-07 18:34:54 +0300

    fix formatters

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,18 @@
 import stylish from './stylish.js';
+import plain from './plain.js';
+import json from './json.js';
 
-const getFormat = (diff, format) => {
-  const formatters = {
-    stylish,
-  };
-  const formatter = formatters[format];
-  return formatter(diff);
+const formatter = (tree, format) => {
+  switch (format) {
+    case 'stylish':
+      return stylish(tree);
+    case 'plain':
+      return plain(tree);
+    case 'json':
+      return json(tree);
+    default:
+      throw new Error(`Unknown format: ${format}`);
+  }
 };
 
-export default getFormat;
+export default formatter;
