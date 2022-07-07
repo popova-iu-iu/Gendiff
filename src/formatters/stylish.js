c18aff6ce22f572d7c176686e73b8f7bf60434fd
iuliia <popova.iuliia.iu@gmail.com>
2022-07-07 18:35:23 +0300

    fix stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,45 +1,60 @@
 import _ from 'lodash';
 
-const space = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
+const space = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat(depth * spacesCount - 2);
 
-const stringify = (tree, depth, stylish) => {
-  if (!_.isObject(tree)) {
-    return String(tree);
+const stringify = (node, depth = 1) => {
+  if (!_.isObject(node)) {
+    return `${node}`;
   }
 
-  const result = Object.entries(tree)
-    .map(([key, value]) => stylish({ type: 'unupdated', key, value }, depth + 1));
-
-  return `{\n${result.join('\n')}\n${space(depth)}  }`;
+  const keys = Object.keys(node);
+  const result = keys.map((key) => {
+    const value = node[key];
+    return `${space(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`
+  })
+  
+  return `{\n${result.join('\n')}\n  ${space(depth)}}`;
 };
 
-const stylish = (node, depth = 0) => {
-  switch (node.type) {
+const stylish = (tree) => {
+  const iter = (node, depth = 0) => {
+    const {
+      key,
+      type,
+      value,
+      children,
+      value1,
+      value2,
+    } = node
+  
+  switch (type) {
     case 'main': {
-      const result = node.children.flatMap((child) => stylish(child, depth + 1));
+      const result = children.flatMap((child) => iter(child, depth + 1));
       return `{\n${result.join('\n')}\n}`;
     }
     case 'nested': {
-      const result = node.children.flatMap((child) => stylish(child, depth + 1));
-      return `${space(depth)}  ${node.key}: {\n${result.join('\n')}\n${space(depth)}  }`;
+      const result = children.flatMap((child) => iter(child, depth + 1));
+      return `${space(depth)}  ${key}: {\n${result.join('\n')}\n${space(depth)}  }`;
     }
-    case 'plus': {
-      return `${space(depth)}+ ${node.key}: ${stringify(node.value, depth, stylish)}`;
+    case 'added': {
+      return `${space(depth)}+ ${key}: ${stringify(value, depth)}`;
     }
-    case 'minus': {
-      return `${space(depth)}- ${node.key}: ${stringify(node.value, depth, stylish)}`;
+    case 'deleted': {
+      return `${space(depth)}- ${key}: ${stringify(node.value, depth)}`;
     }
     case 'unupdated':
-      return `${space(depth)}  ${node.key}: ${stringify(node.value, depth, stylish)}`;
+      return `${space(depth)}  ${key}: ${stringify(value, depth)}`;
     case 'different': {
-      const { key, value1, value2 } = node;
-      const data1 = `${space(depth)}- ${key}: ${stringify(value1, depth, stylish)}`;
-      const data2 = `${space(depth)}+ ${key}: ${stringify(value2, depth, stylish)}`;
+      const data1 = `${space(depth)}- ${key}: ${stringify(value1, depth)}`;
+      const data2 = `${space(depth)}+ ${key}: ${stringify(value2, depth)}`;
       return `${data1}\n${data2}`;
     }
     default:
-      return '';
+      throw new Error(`Unknown type: ${type}`);
   }
+};
+
+  return iter(tree);
 };
 
 export default stylish;
