6c8de5661325b87fcc1bac7ebc98223743f9f05f
iuliia <popova.iuliia.iu@gmail.com>
2022-07-07 18:34:11 +0300

    fix buildDiff

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,38 +1,54 @@
 import _ from 'lodash';
 
 const buildDiff = (object1, object2) => {
-  const keys = _.sortBy(_.union(Object.keys(object1), Object.keys(object2)));
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  const sortedKeys = _.sortBy(_.union(keys1, keys2));
 
-  const result = keys.map((key) => {
+  const result = sortedKeys.map((key) => {
     const value1 = object1[key];
     const value2 = object2[key];
 
     if (!_.has(object1, key)) {
-      return { key, value: value2, type: 'plus' };
+      return {
+        key,
+        value: value2,
+        type: 'added',
+      };
     }
 
     if (!_.has(object2, key)) {
-      return { key, value: value1, type: 'minus' };
+      return {
+        key,
+        value: value1,
+        type: 'deleted',
+      };
     }
 
     if (_.isObject(value1) && _.isObject(value2)) {
-      return { key, children: buildDiff(value1, value2), type: 'nested' };
+      return {
+        key,
+        children: buildDiff(value1, value2),
+        type: 'nested',
+      };
     }
 
     if (!_.isEqual(value1, value2)) {
       return {
-        key, value1, value2, type: 'different',
+        key,
+        value1,
+        value2,
+        type: 'different',
       };
     }
 
-    return { key, value: value1, type: 'unupdated' };
+    return {
+      key,
+      value: value1,
+      type: 'unupdated',
+    };
   });
   return result;
 };
 
-const buildTree = (object1, object2) => {
-  const result = { type: 'main', children: buildDiff(object1, object2) };
-  return result;
-};
-
-export default buildTree;
+export default (object1, object2) => ({ type: 'main', children: buildDiff(object1, object2) });
