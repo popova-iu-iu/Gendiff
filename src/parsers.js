1de8db610e75b92aaa1945749a8bb496679e1d9e
iuliia <popova.iuliia.iu@gmail.com>
2022-07-13 11:14:35 +0300

    fix parsers

--- src/parsers.js
+++ src/parsers.js
@@ -6,8 +6,6 @@ const parsers = (data, format) => {
       return JSON.parse(data);
     case 'yml':
       return yaml.load(data);
-    case 'yaml':
-      return yaml.load(data);
     default:
       throw new Error(`Unknown format: ${format}`);
   }
